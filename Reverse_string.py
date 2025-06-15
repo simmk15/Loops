847154acc44640ee68a108791975a4871e14375a
simmk15 <sknagra15@gmail.com>
2025-06-15 20:37:36 +0530

    Reverse order

---
+++ Reverse_string.py
@@ -0,0 +1,5 @@
+string=input("Enter a string value=")
+revstring=''
+for i in string:
+    revstring=i+revstring
+print("The reversed value="+ revstring)
---
+++ Sum_of_whole_numbers.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a number="))
+i=1
+sum=0
+while i<=num:
+    sum+=i
+    i+=1
+    print("The sum of whole numbers from 1 to",i,"is",sum)
